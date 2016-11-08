81001b3fe00dce0dd51a008fd048088c985dd950
Vincent M. van der Meulen <meulen@uw.edu>
2016-11-08 13:36:29 -0800

    Improved the function

--- js/pagination.js
+++ js/pagination.js
@@ -1,9 +1,13 @@
 (function() {
     "use strict";
 
-    window.onload = function() {
-      var sections = document.querySelectorAll("section");
-    };
-
-    
+    function showSection(section) {
+      visibleSections = document.querySelectorAll(".db");
+      for (var i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("db");
+        sections[i].classList.add("dn");
+      }
+      section.classList.remove("dn");
+      section.classList.add("db");
+    }
 })();
